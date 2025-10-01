7e01c5b0e15396ba61bf28c994c739adafc53580
Guilherme Tamashiro <guitama.games@gmail.com>
2025-10-01 10:13:53 -0300

    1- Criando o projeto

---
+++ src/JS/script.js
@@ -0,0 +1,24 @@
+function validar() {
+    // Pega os valores dos campos
+    let usuario = document.getElementById("usuario").value;
+    let senha = document.getElementById("senha").value;
+
+    // Se qualquer um dos campos estiver vazio, exibe um alerta e para a execução da função.
+    
+
+    // Se os campos estiverem preenchidos, faz a validação do login
+    if (usuario === "Admin") {
+        alert("Bem-vindo!");
+        // Redireciona para a página principal
+        location.href = "home.html";
+
+    } else if (usuario === "" || senha === "") {
+        alert("Por favor, preencha todos os campos.");
+
+    }
+     else {
+        // Se a validação falhar, exibe a mensagem de erro.
+        alert("Usuário e senha inválidos.");
+    }
+
+}
